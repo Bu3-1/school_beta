a3ac00a92f721e4267c0b6664846fc29cb2a473e
Elisa-Reyes <elisa_3141240138@utd.edu.mx>
2026-08-08 23:14:43 -0600

    funciona pls

--- backend/alumnos/src/lib/prisma.js
+++ backend/alumnos/src/lib/prisma.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaClient } from "../generated/client.ts";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
